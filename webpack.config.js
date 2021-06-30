bdde35d948ae42193d5a4dbab031babffc162a05
Pavel Mamatov <pmamatov@icloud.com>
2021-06-30 23:13:48 +0300

    Add Node Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-const NODE_ENV = process.env.NODE_ENV
+let NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
   mode: NODE_ENV || "development",
