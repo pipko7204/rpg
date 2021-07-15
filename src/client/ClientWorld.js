a5db5a1e723988307357db795164919052d1358e
Pavel Mamatov <pmamatov@icloud.com>
2021-07-15 17:57:11 +0300

    hw-2 revisions

--- src/client/ClientWorld.js
+++ src/client/ClientWorld.js
@@ -7,39 +7,31 @@ class ClientWorld {
       height: levelCfg.map.length,
       width: levelCfg.map[0].length,
     });
+
+    this.spritesType = 'grass'
+
   }
+
+
 
   init() {
     for (let spriteX = 0; spriteX < 13; spriteX++) {
       for (let spriteY = 0; spriteY < 13; spriteY++) {
         if (spriteX === 0 || spriteY === 0 || spriteX === 12 || spriteY === 12) {
-          this.engine.renderSpriteFraim({
-            sprite: ['terrain', 'wall'],
-            frame: 0,
-            x: spriteX * 48,
-            y: spriteY * 48,
-            w: 48,
-            h: 48,
-          });
+          this.spritesType = 'wall'
         } else if (spriteY === 8) {
-          this.engine.renderSpriteFraim({
-            sprite: ['terrain', 'water'],
-            frame: 0,
-            x: spriteX * 48,
-            y: spriteY * 48,
-            w: 48,
-            h: 48,
-          });
+          this.spritesType = 'water'
         } else {
-          this.engine.renderSpriteFraim({
-            sprite: ['terrain', 'grass'],
-            frame: 0,
-            x: spriteX * 48,
-            y: spriteY * 48,
-            w: 48,
-            h: 48,
-          });
+          this.spritesType = 'grass'
         }
+        this.engine.renderSpriteFraim({
+          sprite: ['terrain', this.spritesType],
+          frame: 0,
+          x: spriteX * 48,
+          y: spriteY * 48,
+          w: 48,
+          h: 48,
+        });
       }
     }
   }
